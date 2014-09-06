2d64dbc6fffcecf15f82dfa980555a0e7e070fb2
Sankaranarayanan Viswanathan <rationalrevolt@gmail.com>
2014-09-05 21:06:02 -0400

    Added delay to timer

--- src/main/java/com/sankar/drawguess/Timer.java
+++ src/main/java/com/sankar/drawguess/Timer.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Timer extends Thread {
-	
+
 	private Set<Timed> interested = Collections.synchronizedSet(new HashSet<Timed>());
 	
 	public Timer() {
@@ -19,6 +19,10 @@ public class Timer extends Thread {
 	
 	public void unregisterInterest(Timed timed) {
 		interested.remove(timed);
+	}
+	
+	public void schedule(int ticks, Action task) {
+		new Delay(ticks, task).start();
 	}
 
 	@Override
@@ -35,6 +39,57 @@ public class Timer extends Thread {
 				break;
 			}
 		}
+	}
+	
+	public static interface Action {
+		void run();
+	}
+	
+	public class Delay implements Timed {
+		
+		private int ticksRemaining;
+		private Action task;
+		
+		private boolean started;
+		private boolean expired;
+		
+		Delay(int ticks, Action task) {
+			this.ticksRemaining = ticks;
+			this.task = task;
+		}
+
+		@Override
+		public synchronized void tick() {
+			if (!started || expired) throw new IllegalStateException();
+			
+			if (ticksRemaining-- == 0) {
+				expire();
+				runTask();
+			}
+		}
+		
+		public void start() {
+			if (started) return;
+			
+			started = true;
+			registerInterest(this);
+		}
+		
+		public synchronized void cancel() {
+			expire();
+		}
+		
+		private void expire() {
+			if (expired) return;
+			
+			expired = true;
+			unregisterInterest(this);
+		}
+		
+		private void runTask() {
+			task.run();
+		}
+
 	}
 	
 }
